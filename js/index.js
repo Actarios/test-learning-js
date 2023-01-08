c3a1a0e5e4010048cf4e8e85d1bb5a15bba5c5ba
Dimitriy <ivanov-dk@ro.ru>
2023-01-08 20:42:31 +0300

    Adding files

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// const arr = [1, 3, 5, 6];
+
+// console.log(arr)
+
+// // to Number
+
+// let a = parseInt('15px', 10)
+
+// console.log(a)
+
+// let switcher = 10;
+
+// if (switcher) {
+//     console.log('Working...')
+// } else {
+//     console.log('Not working yet...')
+// }
+
+// alert("Hello World");
+
+// let a = prompt("Are you real?", "Yes");
+
+// console.log(a)
+
+// console.log("array" + " is empty");
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+

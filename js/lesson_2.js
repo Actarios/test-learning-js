d185d4566c45348d2ee682565a5deccc57b7d972
Dimitriy <ivanov-dk@ro.ru>
2023-01-11 18:18:22 +0300

    Lesson 2

--- js/lesson_2.js
+++ js/lesson_2.js
@@ -10,13 +10,57 @@ let appData = {
     savings: false
 };
 
+// == 1 ==
+
 for (let i = 0; i < 2; i++) {
     let ans1 = prompt('Введите обязательную статью расходов в этом месяце', '');
-    let ans2 = prompt('Во сколько обойдется?', '');
+    let ans2 = +prompt('Во сколько обойдется?', '');
 
-    appData.expenses[ans1] = ans2;
+    if (    (typeof(ans1) === 'string') &&
+            (typeof(ans2) === 'number') &&
+            (ans1 !== null) && (ans2 !== null) && 
+            (ans1.length != '') &&(ans2.length != '')) {
+        appData.expenses[ans1] = ans2;
+    }
 }
 
-console.log(appData);
+// == 2 ==
 
-alert(`Бюджет на день - ${ appData.budget/30 }`);
+// let count = 0
+
+// while (count < 2) {
+//     let ans1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+//     let ans2 = +prompt('Во сколько обойдется?', '');
+
+//     if (    (typeof(ans1) === 'string') &&
+//             (typeof(ans2) === 'number') &&
+//             (ans1 !== null) && (ans2 !== null) && 
+//             (ans1.length != '') &&(ans2.length != '')) {
+//         appData.expenses[ans1] = ans2;
+//     }
+
+//     count++
+// }
+
+// == 3 ==
+
+// let count = 0
+
+// do {
+//     let ans1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+//     let ans2 = +prompt('Во сколько обойдется?', '');
+
+//     if (    (typeof(ans1) === 'string') &&
+//             (ans1 !== null) && (ans2 !== null) && 
+//             (ans1.length != '') &&(ans2.length != '')) {
+//         appData.expenses[ans1] = ans2;
+//     }
+
+//     count++
+// } while (count < 2)
+
+appData.moneyPerDay = appData.budget / 30
+
+alert(`Бюджет на день - ${appData.moneyPerDay}`);
+
+console.log(appData);

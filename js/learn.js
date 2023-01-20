d768345945687b0ef358d44830c73d37f331304c
Dimitriy <ivanov-dk@ro.ru>
2023-01-20 16:52:27 +0300

    Lesson 9

--- js/learn.js
+++ js/learn.js
@@ -1,17 +1,43 @@
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
+// let timerId = setTimeout(sayHello, 3000)
+// // clearTimeout(timerId)
 
-function multiplyNumeric(obj) {
-    for (let key in obj) {
-        if (typeof obj[key] == 'number') {
-            obj[key] *= 2
+// let timerId = setInterval(sayHello, 3000)
+// clearTimeout(timerId)
+
+// function sayHello() {
+//     console.log('Hello World!')
+// }
+
+// let timerId = setTimeout(function log() {
+//     console.log('Hello!')
+//     setTimeout(log, 2000)
+// })
+
+let btn = document.querySelector('.btn')
+let elem = document.querySelector('.box')
+
+function myAnimation() {
+    let pos = 0
+
+    let id = setInterval(frame, 10)
+    function frame() {
+        if (pos == 300) {
+            clearInterval()
+        } else {
+            pos++
+            elem.style.top = pos + 'px'
+            elem.style.left = pos + 'px'
         }
     }
 }
 
-multiplyNumeric(menu)
+btn.addEventListener('click', myAnimation)
 
-console.log(menu)
+let btnBlock = document.querySelector('.btn-block'),
+    btns = document.getElementsByTagName('button')
+
+btnBlock.addEventListener('click', function(event) {
+    if (event.target && event.target.matches('button.first')) {
+        console.log('Hello')
+    }
+})
